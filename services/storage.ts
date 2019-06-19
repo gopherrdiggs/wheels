6d939576aa5e2239d5e18122175db816794d7125
grodgers <grodgers@GR20170626>
2019-06-19 09:37:57 -0500

    Update storage service

--- services/storage.ts
+++ services/storage.ts
@@ -1,5 +1,43 @@
 class StorageController {
 
+  storage = window.localStorage;
+
+  async set(key: string, value: any) {
+    try {
+      if (!this.storage) {
+        throw new Error('Local storage unavailable.');
+      }
+      this.storage.setItem(key, JSON.stringify(value));
+    }
+    catch (error) {
+      throw new Error(`Unable to store value in local storage: ${value} due to error:` + error.message);
+    }
+  }
+
+  async get(key: string) {
+    try {
+      if (!this.storage) {
+        throw new Error('Local storage unavailable.');
+      }
+      const item = this.storage.getItem(key);
+      return JSON.parse(item);
+    }
+    catch (error) {
+      throw new Error(`Unable to get value from local storage: ${key} due to error:` + error.message);
+    }
+  }
+
+  async remove(key: string) {
+    try {
+      if (!this.storage) {
+        throw new Error('Local storage unavailable.');
+      }
+      this.storage.removeItem(key);
+    }
+    catch (error) {
+      throw new Error(`Unable to remove value from local storage: ${key} due to error:` + error.message);
+    }
+  }
 }
 
 export const Storage = new StorageController();
